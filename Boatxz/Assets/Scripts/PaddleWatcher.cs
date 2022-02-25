4271413940359e1fb3ed90c80e7d51d2c5a8d8f8
Zaid Arshad <zaidyarshad@gmail.com>
2022-02-25 03:41:55 -0800

    Directional velocity based on the force applied by paddles

--- Boatxz/Assets/Scripts/PaddleWatcher.cs
+++ Boatxz/Assets/Scripts/PaddleWatcher.cs
@@ -5,10 +5,25 @@ using UnityEngine;
 public class PaddleWatcher : MonoBehaviour {
     [SerializeField] Rigidbody hull;
     [SerializeField] bool leftPaddle;
+    [SerializeField] int velocityMultiplier;
+    [SerializeField] GameObject player;
+
+    private float oldX;  
+    private float velocity;
+
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.tag == "Water") {
+            oldX = player.transform.InverseTransformPoint(transform.position).x;
+        }
+    }
 
     private void OnTriggerStay(Collider collider) {
+        velocity = -(player.transform.InverseTransformPoint(transform.position).x - oldX);
+        Debug.Log(velocity*5);
+        oldX = player.transform.InverseTransformPoint(transform.position).x;
+
         if (collider.gameObject.tag == "Water") {
-            hull.AddRelativeForce(new Vector3(3f, 0f, 0f), ForceMode.Acceleration);
+            hull.AddRelativeForce(new Vector3(velocity*5, 1f, 0f), ForceMode.Acceleration);
             if (leftPaddle) {
                 hull.AddTorque(0f, 1f, 0f, ForceMode.Acceleration);
             }
